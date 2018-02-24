5aa3311d34bc335f671f6dd0f55ea56b9e466c0b
SwiftyX3 <sousachristian440@gmail.com>
2018-02-24 16:39:03 -0700

    finished reformatting the text files

--- dataprocessing.py
+++ dataprocessing.py
@@ -1,18 +1,33 @@
 #this is the data processing file
 #this file is a small workspace for the data processing fuctions fuctions
 #the fuctions will be inplemented in the datagetter.py file
-def getPosWords(posfile):
-    f=open(posfile)
-    holder[3]
-    for l in f
-        l=l.strip()
-        holder=l.split()
-
-def getNegWords(negfile):
-
-
-def main(): 
-    string posw[1000];
-    string negw[1000];
-     
+def process(raw_tweet):
     
+def main(): 
+    posw =[None]* 2005
+    negw= [None]* 2005
+    ignorew =[None]*50
+    count =0
+    test_string= " I like pizza of all kinds except pinnaple, I hate pinnale pizza. It is truly awful. But what I really like is ice cream. its the best."
+    f=open("poswords.txt", "r")
+    for l in f:
+        posw[count]=l
+        count+=1
+    f.close()
+    count=0
+    f= open("negwords.txt")
+    for l in f:
+        negw[count]=l
+        count+=1
+    f.close()
+    count=0
+    f=open("ignoreWords.txt")
+    for l in f:
+        ignorew[count]=l
+        count+=1
+    f.close()
+    print("files have been read")
+
+
+if __name__ == "__main__":
+    main()
--- remap.cpp
+++ remap.cpp
@@ -1,7 +1,11 @@
 // this file is used to copy and paste from one flie to another for formatting
+// this is verry ineffcient but i dont have a faster way of reformatting the file the way i need it
+// to be.
 #include<iostream>
 #include<sstream>
 #include<fstream>
+#include<stdlib.h>
+#include<time.h>
 using namespace std;
 
 void countwords()
@@ -22,7 +26,38 @@ void countwords()
         }
         index++;
     }
+    datafile.close();
+    int num;
+    cout<< "#"<< endl;
+    cout<< "file has been read"<< endl;
+    cout<< "#"<< endl;
+     for(int i=0;i<386; i++)
+    {
+        srand(time(NULL));
+        num= rand() % 386+1;
+        if(array[num]!="")
+        {
+            cout<< "editing item "<<i<< " of 368" << endl;
+            array[num]="";
+        }
+        else
+        {
+            i--;
+        }
 
+    }
+    cout<< "#"<< endl;
+    cout<< "array editing complete"<< endl;
+    cout<< "#"<< endl;
+    cout<< "writing to negwords.txt"<<endl;
+    ofstream outfile("negwords.txt");
+    for(int i=0;i<2391;i++)
+    {
+        if(array[i]!="")
+        {
+            outfile<<array[i]<<endl;
+        }
+    }
 }
 
 
