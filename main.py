7d7a209e515a90fb9f25c5f0ba780fa53ea29b48
Luke <lucas.a.lyon@gmail.com>
2018-02-25 10:35:34 -0700

    added 'features'

--- main.py
+++ main.py
@@ -1,14 +1,71 @@
 import tweepy
 import dataset
 import os
+from datafreeze import freeze
+import re
+import csv
 import json
+import time
 from html.parser import HTMLParser
+
+
+
 database = dataset.connect("sqlite:///tweets.db")
 table = database["tweets"]
+
+#creating empty arrays to hold good, bad, and ignore words from text files
+posw =[None]* 2012
+negw= [None]* 2020
+ignorew =[None]*51
+
+
+count = 0
+#adding the good words
+f=open("poswords.txt", "r")
+for l in f:
+    l=l.strip()
+    posw[count]=l
+    count+=1
+f.close()
+count=0
+#adding the bad words
+f= open("negwords.txt")
+for l in f:
+    l=l.strip()
+    negw[count]=l
+    count+=1
+f.close()
+count=0
+#adding the ignore words
+f=open("ignoreWords.txt")
+for l in f:
+    l=l.strip()
+    ignorew[count]=l
+    count+=1
+f.close()
+
+time_start = time.time()
+
+#dictionary of cities and their tweet values
+city_scores = dict()
+
+def csvReader():
+    with open('tweets.csv', 'r') as file:
+        has_header = csv.Sniffer().has_header(file.read(1024))
+        file.seek(0)
+        reader = csv.reader(file)
+        if has_header:
+            next(reader)
+        for row in reader:
+            cleaned_tweet = process(row[1])
+            tweetloc = row[2]
+            score = scorer(cleaned_tweet, tweetloc)
+
 def process(raw_tweet):
     raw_tweet= re.sub(r'[^\w\s]','',raw_tweet)
     raw_tweet=raw_tweet.lower()
     return raw_tweet
+
 def scorer(tweet, location):
     positive=0
     negative=0
@@ -31,12 +88,79 @@ def scorer(tweet, location):
     	city_scores[location] = [value]
     return value
 
+
+
+def dumper():
+    #dump the database into the csv file
+    database = dataset.connect("sqlite:///tweets.db")
+
+    result = database["tweets"].all()
+
+    #tweets.db can not be empty at this point!
+    freeze(result, format='csv', filename="tweets.csv")
+
+    table = database["tweets"]
+    table.delete()
+
+    #analyze each line in the csv file
+    csvReader()
+
+    avg_city_scores = {}
+
+
+    for key, value in city_scores.items():
+        avg_city_scores[key] = (sum(value)/ float(len(value)))
+    print("###########################################")
+    print(avg_city_scores)
+
+    scores = ""
+
+    for city in cities:
+        if city in avg_city_scores:
+            if avg_city_scores[city] < -0.5:
+                s = 0
+            elif (-0.5 < avg_city_scores[city] < 0):
+                s = 1
+            elif (avg_city_scores[city] == 0):
+                s = 2
+            elif (0 < avg_city_scores[city] < 0.5):
+                s = 3
+            else:
+                s = 4
+            scores += str(s)
+            scores += '\n'
+        else:
+            scores += '0\n'
+
+    with open('scores.txt', 'w') as file:
+        file.write(scores)
+
+    
+    city_scores.clear()
+
+    os.remove("tweets.csv")
+
+    #erase the database tweets table
+
+    
+
+    try:
+        os.delete("tweets.csv")
+    except:
+        pass
+    #wait for five minutes, then redo the above code
+
 #List of cities to check against status.user.location
 cities = ["Denver", "Colorado Springs", "Aurora", "Fort Collins", "Lakewood", "Thorton", "Pueblo", "Arvada", "Westminster", "Centennial", "Boulder"]
 
 class StreamListener(tweepy.StreamListener):
-	
+
+    
     def on_status(self, status):
+        global time_start
+        if ((time.time() - time_start) >600):
+            dumper()
+            time_start = time.time()
 		#overriding the listener class method "on_status" to focus on tweets sent, not dm's or deleting tweets
         if status.retweeted:
             return
@@ -88,3 +212,19 @@ stream_listener = StreamListener()
 stream = tweepy.Stream(auth=api.auth, listener=stream_listener, tweet_mode='extended')
 
 stream.filter(locations = ColoradoGeoBox, languages=["en"])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
