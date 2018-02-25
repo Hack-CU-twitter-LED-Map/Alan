9de6941d60750b366dcaabcfad488312fe803a10
lalyon <lucas.a.lyon@gmail.com>
2018-02-24 21:08:10 -0700

    updated with csc functionality

--- dataprocessing.py
+++ dataprocessing.py
@@ -3,54 +3,35 @@
 #the fuctions will be inplemented in the datagetter.py file
 import re
 import time
+import csv
+
+def csvReader():
+    with open('tweets.csv', 'r') as file:
+        has_header = csv.Sniffer().has_header(file.read(1024))
+        file.seek(0)
+        reader = csv.reader(file)
+        if has_header:
+            next(reader)
+        for row in reader:
+            cleaned_tweet = process(row[1])
+            print(cleaned_tweet, scorer(cleaned_tweet))
+
 def process(raw_tweet):
     raw_tweet= re.sub(r'[^\w\s]','',raw_tweet)
     raw_tweet=raw_tweet.lower()
     return raw_tweet
 
-def main(): 
-    start_time=time.time()
+def scorer(tweet):
     positive=0
     negative=0
-    posw =[None]* 2012
-    negw= [None]* 2020
-    ignorew =[None]*51
-    count =0
-    test_string= "#IWasOnTheDanceFloorWhen re-creating that jump-and-lift scene from Dirty Dancing seemed like a fun idea. Nope, wasn't...but I did meet some rather nice paramedics"
-    f=open("poswords.txt", "r")
-    for l in f:
-        l=l.strip()
-        posw[count]=l
-        count+=1
-    f.close()
-    count=0
-    f= open("negwords.txt")
-    for l in f:
-        l=l.strip()
-        negw[count]=l
-        count+=1
-    f.close()
-    count=0
-    f=open("ignoreWords.txt")
-    for l in f:
-        l=l.strip()
-        ignorew[count]=l
-        count+=1
-    f.close()
-    tweet=process(test_string)
     tweet=tweet.split()
     for i in tweet:
         if i in ignorew:
             continue
         elif i in posw:
-            print("pos",i)
             positive+=1
         elif i in negw:
-            print("neg",i)
             negative-=1
-    print(negative)
-    print(positive)
-    print ("Program Excicuted in ", time.time()-start_time)
     if positive>negative:
         return 1
     if negative>positive:
@@ -58,8 +39,40 @@ def main():
     else:
         return 0
 
-        
-     
+
+
+posw =[None]* 2012
+negw= [None]* 2020
+ignorew =[None]*51
+count = 0
+f=open("poswords.txt", "r")
+for l in f:
+    l=l.strip()
+    posw[count]=l
+    count+=1
+f.close()
+count=0
+
+f= open("negwords.txt")
+for l in f:
+    l=l.strip()
+    negw[count]=l
+    count+=1
+f.close()
+count=0
+
+f=open("ignoreWords.txt")
+for l in f:
+    l=l.strip()
+    ignorew[count]=l
+    count+=1
+f.close()
+
+def main(): 
+
+    csvReader()
+
+    
 
 
 if __name__ == "__main__":
